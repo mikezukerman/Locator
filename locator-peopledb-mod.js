6fa7713b0803b87ad6a2e81142935bcf1df3fb46
mikez <mike.zukerman@gmail.com>
2016-02-22 19:22:50 -0500

    More conn tweaking

--- locator-peopledb-mod.js
+++ locator-peopledb-mod.js
@@ -2,10 +2,22 @@ var config = require('./config.js');
 var MongoClient = require('mongodb').MongoClient;
 
 var dbConnection;
-var url = 'mongodb://' + (process.env.OPENSHIFT_MONGODB_DB_HOST || config.MONGO_DB_URL);
+//var connString = 'mongodb://' + (process.env.OPENSHIFT_MONGODB_DB_HOST || config.MONGO_DB_URL);
+
+// default to a 'localhost' configuration:
+var connectionSring = 'mongodb://127.0.0.1:27017/locator';
+
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connectionSring = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
 var collectionName = config.MONGO_DB_COLLECTION;
 
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(connectionSring, function(err, db) {
   if (err == null) {
     console.log("Connected to server.");
     dbConnection = db;    
